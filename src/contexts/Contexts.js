2aebbe4334fbb08a0f9a0a21d5676145d36d4451
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-07-08 12:52:52 +0300

    add Contexts

---
+++ src/contexts/Contexts.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+export const CurrenciesContext = createContext([]);
+export const IsLoadingContext = createContext(true);
+export const IsErrorContext = createContext(false);
