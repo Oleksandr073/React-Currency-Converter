0c885a6c8686374234884f11babf156b9da95fac
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-07-08 12:49:32 +0300

    update GlobalStyle

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
+import 'modern-normalize';
 
 const GlobalStyle = createGlobalStyle`
     body {
@@ -6,10 +7,19 @@ const GlobalStyle = createGlobalStyle`
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+        color: #2a2a2a;
+        background-color: #F6F1F1;
     }
 
-    code {
-        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+    p, h1, h2, h3, h4, h5, h6 {
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
     }
 `;
 
