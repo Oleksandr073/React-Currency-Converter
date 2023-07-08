38cbf5a9e1ba591db9f04e08b92197db9f325e00
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-07-08 15:38:05 +0300

    add useMemo for optionValues

--- src/components/CurrencySelectInput/CurrencySelectInput.js
+++ src/components/CurrencySelectInput/CurrencySelectInput.js
@@ -1,9 +1,13 @@
+import { useMemo } from 'react';
 import NumericInput from '../NumericInput/NumericInput';
 import Select from '../Select/Select';
 import { SelectInputWrapper } from './CurrencySelectInput.styled';
 
 function CurrencySelectInput({ currency, value, optionValues, selectHandler, inputHandler }) {
-    const options = optionValues.map(value => ({ value, label: value, }));
+    const options = useMemo(
+        () => optionValues.map(value => ({ value, label: value, })),
+        [optionValues]
+    );
 
     return (
         <SelectInputWrapper>
