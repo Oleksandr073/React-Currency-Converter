ba9799f36d4a9bfb73e33e71052fb990dd533e3f
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-07-08 12:55:33 +0300

    add providers and routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import { getCurrencies } from './services/currencyService';
+import MainLayout from './layout/MainLayout';
+import MainPage from './pages/MainPage';
+import NotFoundPage from './pages/NotFoundPage';
+import useFetch from './hooks/useFetch';
+import AppProviders from './providers/AppProviders';
+
 function App() {
+    const { data: currencies, isLoading, isError } = useFetch(getCurrencies);
     return (
-        <></>
+        <AppProviders currencies={currencies} isLoading={isLoading} isError={isError} >
+            <MainLayout>
+                <Routes>
+                    <Route path='/' element={<MainPage />} />
+                    <Route path='*' element={<NotFoundPage />} />
+                </Routes>
+            </MainLayout>
+        </AppProviders>
     );
 }
 
