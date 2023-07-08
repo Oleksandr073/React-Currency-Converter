716aa2564e4eb16b2b01135394dac7ae3504bdc6
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-07-08 16:30:52 +0300

    add useMemo for optionValues

--- src/components/ConversionSection/ConversionSection.js
+++ src/components/ConversionSection/ConversionSection.js
@@ -1,14 +1,29 @@
+import { CurrenciesContext } from '../../contexts/Contexts';
+import { useContext } from 'react';
 import ConversionSectionInner from '../ConversionSectionInner/ConversionSectionInner';
 import { Wrapper, StyledContainer } from './ConversionSection.styled';
+import Loader from '../Loader/Loader';
+import Error from '../Error/Error';
+import NoCurrenciesMsg from '../NoCurrenciesMsg/NoCurrenciesMsg';
 
 function ConversionSection() {
     return (
         <Wrapper>
             <StyledContainer>
-                <ConversionSectionInner />
+                <ConversionSectionContent />
             </StyledContainer>
         </Wrapper>
     );
+}
+
+function ConversionSectionContent() {
+    const { currencies, isLoading, isError } = useContext(CurrenciesContext);
+    
+    if (isLoading) return <Loader color='#19A7CE' size={70} />;
+    if (isError) return <Error color='#19A7CE' size={200} />;
+    if (!currencies?.length) return <NoCurrenciesMsg />;
+
+    return <ConversionSectionInner currencies={currencies} />
 }
 
 export default ConversionSection;
--- src/components/ConversionSectionInner/ConversionSectionInner.js
+++ src/components/ConversionSectionInner/ConversionSectionInner.js
@@ -1,53 +1,56 @@
-import { useState, useContext } from 'react';
-import { CurrenciesContext } from '../../contexts/Contexts';
+import { useState, useMemo } from 'react';
 import ConversionForm from '../ConversionForm/ConversionForm';
-import Loader from '../Loader/Loader';
-import Error from '../Error/Error';
-import NoCurrenciesMsg from '../NoCurrenciesMsg/NoCurrenciesMsg';
 
-function ConversionSectionInner() {
-    const { currencies, isLoading, isError } = useContext(CurrenciesContext);
-
+function ConversionSectionInner({ currencies }) {
     const [firstCurrency, setFirstCurrency] = useState('USD');
     const [secondCurrency, setSecondCurrency] = useState('UAH');
     const [value, setValue] = useState(1);
     const [isCurrentFirstInput, setIsCurrentFirstInput] = useState(true);
 
-    if (isLoading) return <Loader color='#19A7CE' size={70} />;
-    if (isError) return <Error color='#19A7CE' size={200} />;
-    if (!currencies?.length) return <NoCurrenciesMsg />;
+    const { first, second } = useMemo(
+        () => {
+            const optionValues = currencies.map(({ cc }) => cc);
 
-    const optionValues = currencies.map(({ cc }) => cc);
+            const firstOptionValues = optionValues.filter(optionValue => optionValue !== secondCurrency);
+            const secondOptionValues = optionValues.filter(optionValue => optionValue !== firstCurrency);
 
-    const firstOptionValues = optionValues.filter(optionValue => optionValue !== secondCurrency);
-    const secondOptionValues = optionValues.filter(optionValue => optionValue !== firstCurrency);
+            const firstCurrencyObj = currencies.find(({ cc }) => cc === firstCurrency);
+            const secondCurrencyObj = currencies.find(({ cc }) => cc === secondCurrency);
 
-    const firstCurrencyObj = currencies.find(({ cc }) => cc === firstCurrency);
-    const secondCurrencyObj = currencies.find(({ cc }) => cc === secondCurrency);
+            return {
+                first: {
+                    optionValues: firstOptionValues,
+                    ...firstCurrencyObj
+                },
+                second: {
+                    optionValues: secondOptionValues,
+                    ...secondCurrencyObj
+                },
+            };
+        },
+        [currencies, firstCurrency, secondCurrency]
+    );      
 
-    const { rate: firstRate, txt: firstText, cc: firstCurrencyCode } = firstCurrencyObj;
-    const { rate: secondRate, txt: secondText, cc: secondCurrencyCode } = secondCurrencyObj;
-
-    const firstValue = (isCurrentFirstInput ? value : value * secondRate / firstRate);
-    const secondValue = (!isCurrentFirstInput ? value : value * firstRate / secondRate);
+    const firstValue = (isCurrentFirstInput ? value : value * second.rate / first.rate);
+    const secondValue = (!isCurrentFirstInput ? value : value * first.rate / second.rate);
 
     return (
         <>
             <ConversionForm
-                countryCode={firstCurrencyCode.slice(0, -1)}
-                currencyText={firstText}
+                countryCode={first.cc.slice(0, -1)}
+                currencyText={first.txt}
                 currency={firstCurrency}
                 value={firstValue}
-                optionValues={firstOptionValues}
+                optionValues={first.optionValues}
                 selectHandler={value => setFirstCurrency(value)}
                 inputHandler={value => { setValue(value); setIsCurrentFirstInput(true); }}
             />
             <ConversionForm
-                countryCode={secondCurrencyCode.slice(0, -1)}
-                currencyText={secondText}
+                countryCode={second.cc.slice(0, -1)}
+                currencyText={second.txt}
                 currency={secondCurrency}
                 value={secondValue}
-                optionValues={secondOptionValues}
+                optionValues={second.optionValues}
                 selectHandler={value => setSecondCurrency(value)}
                 inputHandler={value => { setValue(value); setIsCurrentFirstInput(false); }}
             />
