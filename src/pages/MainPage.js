0a311d6f1bf11b2038a4e9cf323d7f8c294ed0c2
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-07-08 12:52:10 +0300

    add MainPage page

---
+++ src/pages/MainPage.js
@@ -0,0 +1,9 @@
+import ConversionSection from "../components/ConversionSection/ConversionSection";
+
+function MainPage() {
+    return (
+        <ConversionSection />
+    );
+}
+
+export default MainPage;
