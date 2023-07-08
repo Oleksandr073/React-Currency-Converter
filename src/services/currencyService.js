98b656dd38babc422c9c5d1dfa16c88447aea354
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-07-08 12:52:01 +0300

    add currencyService

---
+++ src/services/currencyService.js
@@ -0,0 +1,28 @@
+const BASE_URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+
+export async function getCurrencies() {
+    const url = BASE_URL;
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    
+    data.unshift({
+        txt: 'Українська гривня',
+        rate: 1,
+        cc: 'UAH',
+    });
+
+    const currencies = data.filter(({ cc }) =>
+        cc === 'UAH' ||
+        cc === 'EUR' ||
+        cc === 'USD' ||
+        cc === 'PLN' ||
+        cc === 'KZT'
+    ).map(({ txt, rate, cc }) => ({ txt, rate, cc }));
+
+    return currencies;
+}
